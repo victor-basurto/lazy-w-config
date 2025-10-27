c7b01a13024ecf427cf8d6b526b10c69e0d15be9
Victor Basurto <my.mvplace@gmail.com>
2025-10-27 13:21:08 -0700

    feat: Add friendly-snippets to LuaSnip

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -42,4 +42,14 @@ return {
     },
     event = "InsertEnter",
   },
+  {
+    "L3MON4D3/LuaSnip",
+    event = "InsertEnter",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 }
